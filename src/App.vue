8c7ff8162399b1395f3a54fbce958437daba9ea2
Wesley Freitas <wesley8bu@gmail.com>
2023-10-02 21:02:01 -0300

    primeiros passos do commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <input type="text" placeholder="busca pokemon pelo nome" v-model="busca" class="input is-rounde">
+    <button class="button is-fullwidth is-success" @click="buscar" id="buscaBtn">Buscar</button>
+
+    <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
+<Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import Pokemon from "./components/Pokemon"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  data() {
+    return {
+      pokemons: [],
+      filteredPokemons:[],
+      busca:''
+    };
+  },
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        this.pokemons = res.data.results
+        this.filteredPokemons = res.data.results
+      });
+  },
+  components : {
+    Pokemon
+  },
+  methods: {
+    buscar:function(){
+      this.filteredPokemons = this.pokemons;
+    if(this.busca == "" || this.busca == " "){
+      this.filteredPokemons = this.pokemons;
 
+      }else{
+      this.filteredPokemons = this.pokemons.filter(pokemon =>pokemon.name == this.busca)
+
+      }
+    }
+    },
+  computed:{
+    resultadoBusca: function (){
+      if(this.busca == "" || this.busca == " "){
+return this.pokemons;
+      }else{
+return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
+  }
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,5 +65,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#buscaBtn{
+margin-top:2%;
 }
 </style>
